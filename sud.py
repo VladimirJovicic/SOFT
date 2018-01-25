ff905cf255d5ccb7e05b6d09ed573547bff4e708
Vladimir <vlada.jova@yahoo.com>
2018-01-25 14:13:48 +0100

    prvi komit

---
+++ sud.py
@@ -0,0 +1,70 @@
+import homography
+import cv2
+import numpy as np
+
+def angle_cos(p0, p1, p2):
+    d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
+    return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
+
+def find_squares(img):
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    squares = []
+    for gray in cv2.split(img):
+        for thrs in xrange(0, 255, 26):
+            if thrs == 0:
+                bin = cv2.Canny(gray, 0, 50, apertureSize=5)
+                bin = cv2.dilate(bin, None)
+            else:
+                _retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
+            bin, contours, _hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            for cnt in contours:
+                cnt_len = cv2.arcLength(cnt, True)
+                cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
+                if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
+                    cnt = cnt.reshape(-1, 2)
+                    max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
+                    if max_cos < 1:
+                        squares.append(cnt)
+    return squares
+
+
+
+img = cv2.imread('C:\Users\Vladimir\Pictures\proba.png')
+img = cv2.GaussianBlur(img,(5,5),0)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+mask = np.zeros((gray.shape),np.uint8)
+kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11))
+
+close = cv2.morphologyEx(gray,cv2.MORPH_CLOSE,kernel1)
+div = np.float32(gray)/(close)
+res = np.uint8(cv2.normalize(div,div,0,255,cv2.NORM_MINMAX))
+res2 = cv2.cvtColor(res,cv2.COLOR_GRAY2BGR)
+
+thresh = cv2.adaptiveThreshold(res,255,0,1,201,2)
+_, contours, hierarch = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#alternativa je bila
+#thresh = cv22.adaptiveThreshold(res,255,0,1,19,2)
+#contour,hier = cv22.findContours(thresh,cv22.RETR_TREE,cv22.CHAIN_APPROX_SIMPLE)
+#ali je bacao neku gresku koju smo pronasli na linku :
+#https://github.com/jasonlfunk/ocr-text-extraction/issues/5
+
+max_area = 0
+best_cnt = None
+for cnt in contours:
+    area = cv2.contourArea(cnt)
+    if area > 1000:
+        if area > max_area:
+            max_area = area
+            best_cnt = cnt
+
+
+cv2.drawContours(mask,[best_cnt],0,255,-1)
+cv2.drawContours(mask,[best_cnt],0,0,0)
+res = cv2.bitwise_and(res,mask)
+
+squares = find_squares(img)
+cv2.drawContours( img, squares, -1, (0, 255, 255), 3 )
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
